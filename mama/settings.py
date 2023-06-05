693364ddbaa8477da9a8700b42a09de5950939d7
TemirlanSadykov <temirlansadykov2004@gmail.com>
2023-06-05 22:55:19 +0900

    CSRF

--- mama/settings.py
+++ mama/settings.py
@@ -132,3 +132,5 @@ STATICFILES_DIR = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://aktisserver-production.up.railway.app']
